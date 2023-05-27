0eb785d7d0db9f53c1c2fd2e7fe8a847cdb39158
saturnesegithub <134809658+saturnesegithub@users.noreply.github.com>
2023-05-27 21:14:18 +0100

    Add files via upload

---
+++ useless.py
@@ -0,0 +1,20 @@
+def text(text):
+    print(text)
+def put(input):
+    input(input)
+def plus(a,b):
+    print(a + b)
+def minus(a,b):
+    print(a - b)
+def mult(a,b):
+    print(a * b)
+def div(a,b):
+    print(a / b)
+def power(a,b):
+    print(a**b)
+def pi(radius):
+    print(2*3.14*radius)
+def strictlist(a,b,c,d,e,f):
+    [a,b,c,d,e,f]
+    if (a)(b)(c)(d)(e)(f)== 0:
+        print("useless.list//error: Must input 6 elements,not >6 or 6<")
